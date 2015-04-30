d6b9486b23f5c31f9bb6c7a36456baa16de39933
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-04-30 15:59:19 +0200

    added googlenet image test

---
+++ playground/googlenet-test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import caffe
+import os
+
+caffe_root = os.environ['CAFFE_ROOT']
+
+# Set the right path to your model definition file, pretrained model weights,
+# and the image you would like to classify.
+MODEL_FILE = caffe_root + '/models/bvlc_googlenet/deploy.prototxt'
+PRETRAINED = caffe_root + '/models/bvlc_googlenet/bvlc_googlenet.caffemodel'
+IMAGE_FILE = 'images/cat.jpg'
+
+if not os.path.isfile(PRETRAINED):
+    print("Did not find pretrained model file.")
+
+
+caffe.set_mode_cpu()
+net = caffe.Classifier(MODEL_FILE, PRETRAINED,
+                       mean=np.load(caffe_root + '/python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1),
+                       channel_swap=(2,1,0),
+                       raw_scale=255,
+                       image_dims=(256, 256))
+
+input_image = caffe.io.load_image(IMAGE_FILE)
+#plt.imshow(input_image)
+#plt.show()
+
+prediction = net.predict([input_image])  # predict takes any number of images, and formats them for the Caffe net automatically
+print 'prediction shape:', prediction[0].shape
+plt.plot(prediction[0])
+print 'predicted class:', prediction[0].argmax()
+plt.show()
+
+# load labels
+imagenet_labels_filename = caffe_root + '/data/ilsvrc12/synset_words.txt'
+labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
+
+# sort top k predictions from softmax output
+top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
+print labels[top_k]
