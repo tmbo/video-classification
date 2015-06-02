ab679421ae88873c9f031f3ffee5d4af282639fa
Stefan Bunk <stefan.bunk@googlemail.com>
2015-06-02 11:15:56 +0200

    Fix train_activity_recognition.

--- nets/activity_recognition/train_activity_recognition.sh
+++ nets/activity_recognition/train_activity_recognition.sh
@@ -3,11 +3,9 @@
 export CAFFE_ROOT="$HOME/caffe-tmbo"
 
 WEIGHTS=$CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
-#    -weights $WEIGHTS \
 
 $CAFFE_ROOT/build/tools/caffe train \
-    -solver $CAFFE_ROOT/examples/activity_recognition/solver.prototxt \
+    -solver $MP_HOME/nets/activity_recognition/solver.prototxt \
     -weights $WEIGHTS 2>&1 | tee uc101.tlog | less
 #    -snapshot snapshots/_iter_50000.solverstate
-#    -gpu 0 2>&1 | less
 
