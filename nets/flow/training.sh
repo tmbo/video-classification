04bba2508d7f6539c115ffb05268258b96eb2ecd
mpss2015 <mpss2015@hpi.de>
2015-06-09 18:24:01 +0200

    Remove old data.

--- nets/flow/training.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/caffe-tmbo
-
-./build/tools/caffe train --solver=examples/flow/solver.prototxt --weights=models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel > examples/flow/output 2>&1
-
