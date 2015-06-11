1d9dccc28622606ad41e1d2a955cc9b25c321200
Stefan Bunk <stefan.bunk@googlemail.com>
2015-06-11 16:10:39 +0200

    Bash scripting!

--- nets/activity_recognition/train_activity_recognition.sh
+++ nets/activity_recognition/train_activity_recognition.sh
@@ -11,6 +11,8 @@ FOLDER_NAME="${DATE}_$1"
 TRAINING_LOG_NAME="uc101.tlog"
 echo "Saving experiment in experiments/$FOLDER_NAME"
 
+rm snapshots/*
+
 mkdir experiments/$FOLDER_NAME
 cp net.prototxt solver.prototxt deploy.prototxt train_activity_recognition.sh experiments/$FOLDER_NAME
 
@@ -20,11 +22,15 @@ WEIGHTS=$CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffe
 
 $CAFFE_ROOT/build/tools/caffe train \
     -solver $MP_HOME/nets/activity_recognition/solver.prototxt \
-    -weights $WEIGHTS 2>&1 > $TRAINING_LOG_NAME
+    -weights $WEIGHTS 2> $TRAINING_LOG_NAME
 #    -snapshot snapshots/_iter_50000.solverstate
 
-cp -r snapshots/ experiments/$FOLDER_NAME
+echo "Training finished."
+echo "Copying snapshots .."
+ls -v -1 snapshots/ | tail -n 2 | xargs -i mv snapshots/{} experiments/$FOLDER_NAME
+echo "Parsing logs"
 $CAFFE_ROOT/tools/extra/parse_log.sh $TRAINING_LOG_NAME
+echo "Building plots"
 gnuplot -e "filename='$TRAINING_LOG_NAME'" -p plot_log.gnuplot
 
 mv *.png experiments/$FOLDER_NAME
