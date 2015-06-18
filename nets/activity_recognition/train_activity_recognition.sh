6d822ab381dedd2b4534b4cf0282811880827bae
mpss2015 <mpss2015@hpi.de>
2015-06-18 12:07:42 +0200

    Adapt train script to experiment setting

--- nets/activity_recognition/train_activity_recognition.sh
+++ nets/activity_recognition/train_activity_recognition.sh
@@ -11,22 +11,27 @@ FOLDER_NAME="${DATE}_$1"
 TRAINING_LOG_NAME="uc101.tlog"
 echo "Saving experiment in experiments/$FOLDER_NAME"
 
-rm snapshots/*
+rm snapshots/* 2> /dev/null
 
 mkdir experiments/$FOLDER_NAME
-cp net.prototxt solver.prototxt deploy.prototxt train_activity_recognition.sh experiments/$FOLDER_NAME
+
+cp net.prototxt solver.prototxt train_activity_recognition.sh experiments/$FOLDER_NAME
 
 export CAFFE_ROOT="$HOME/caffe-tmbo"
 
-WEIGHTS=$CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
-
+#WEIGHTS=$CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
+WEIGHTS=$CAFFE_ROOT/models/CNN_M_2048/VGG_CNN_M_2048.caffemodel
 $CAFFE_ROOT/build/tools/caffe train \
     -solver $MP_HOME/nets/activity_recognition/solver.prototxt \
     -weights $WEIGHTS 2> $TRAINING_LOG_NAME
 #    -snapshot snapshots/_iter_50000.solverstate
-
-echo "Training finished."
-echo "Copying snapshots .."
+if [ $? -ne 0 ]; then
+    echo "Training not successful. Exiting."
+    exit 2
+fi
+     
+echo "Training finished"
+echo "Copying snapshots"
 ls -v -1 snapshots/ | tail -n 2 | xargs -i mv snapshots/{} experiments/$FOLDER_NAME
 echo "Parsing logs"
 $CAFFE_ROOT/tools/extra/parse_log.sh $TRAINING_LOG_NAME
