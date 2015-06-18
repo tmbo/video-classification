3edc2348d4b050eaace457336aec2be2a9f2ffa6
mpss2015 <mpss2015@hpi.de>
2015-06-18 17:04:47 +0200

    Adapt flow nets.

--- nets/flow/training.sh
+++ nets/flow/training.sh
@@ -1,45 +1,67 @@
 #!/usr/bin/env bash
 
-DATE=`date +%Y%m%d-%H%M%S`
+# Check if called with name
 if [ $# -ne 2 ]; then
-  echo "Usage: $0 [experiment_name] [architecture]"
-	echo "   experiment_name: Name of the subfolder in ./experiments/ for the current experiment."
-  echo "   architecture:    Subfolder in . that contains the network definition."
+    echo "Usage: $0 [experiment_name] [architecture]"
+	echo "       experiment_name: Name of the subfolder in ./experiments/ for the current experiment."
+    echo "       architecture:    Subfolder in . that contains the network definition."
 	echo "Exiting."
 	exit 1
 fi
+
+# Set Vars
+DATE=`date +%Y%m%d-%H%M%S`
 FOLDER_NAME="${DATE}_$1"
 ARCH="$2"
 TRAINING_LOG_NAME="uc101.tlog"
+
 echo "Saving experiment in experiments/$FOLDER_NAME"
-PLOT=0
-
-rm snapshots/* 2> /dev/null
-
 mkdir experiments/$FOLDER_NAME
 
-cp $ARCH/net.prototxt $ARCH/solver.prototxt training.sh experiments/$FOLDER_NAME
-cd experiments/$FOLDER_NAME
+# Function for saving results and making plots
+function cleanup() {
+    echo $1
+    
+    echo "Copying snapshots"
+    ls -v -1 snapshots/ | tail -n 2 | xargs -i mv snapshots/{} experiments/$FOLDER_NAME
+    
+    echo "Parsing logs"
+    $CAFFE_ROOT/tools/extra/parse_log.sh $TRAINING_LOG_NAME
+    
+    echo "Copying logs"
+    cp $TRAINING_LOG_NAME $TRAINING_LOG_NAME.train $TRAINING_LOG_NAME.test experiments/$FOLDER_NAME
+    
+    echo "Building plots"
+    gnuplot -e "filename='$TRAINING_LOG_NAME'" -p plot_log.gnuplot
+    mv *.png experiments/$FOLDER_NAME
+    
+    rm ${TRAINING_LOG_NAME}.test ${TRAINING_LOG_NAME}.train
+    echo "Clean up finished"
+}
 
-export CAFFE_ROOT="$HOME/caffe-tmbo"
+# Clean snapshots
+rm snapshots/* 2> /dev/null
+
+# Saving setup
+cp $ARCH/net.prototxt $ARCH/solver.prototxt training.sh experiments/$FOLDER_NAME
+
+# Setting interrupt trap
+trap 'cleanup "Training interrupted"; exit 1' INT
+
+# Calling caffe
+# export CAFFE_ROOT="$HOME/caffe-tmbo"
 
 $CAFFE_ROOT/build/tools/caffe train \
-    -solver $MP_HOME/nets/flow/experiments/$FOLDER_NAME/solver.prototxt | less #2> $TRAINING_LOG_NAME
-#    -snapshot snapshots/_iter_50000.solverstate
+    -solver $MP_HOME/nets/flow/experiments/$FOLDER_NAME/solver.prototxt 2> $TRAINING_LOG_NAME
+
+# Resetting interrupt handling
+trap - INT
+
+# Check if Training successful
 if [ $? -ne 0 ]; then
     echo "Training not successful. Exiting."
     exit 2
 fi
-if [ $PLOT -ne 0 ]; then
-  cd ../..
-  echo "Training finished"
-  echo "Copying snapshots"
-  ls -v -1 snapshots/ | tail -n 2 | xargs -i mv snapshots/{} experiments/$FOLDER_NAME
-  echo "Parsing logs"
-  $CAFFE_ROOT/tools/extra/parse_log.sh experiments/$FOLDER_NAME/$TRAINING_LOG_NAME
-  echo "Building plots"
-  gnuplot -e "filename='$TRAINING_LOG_NAME'" -p plot_log.gnuplot
 
-  mv *.png experiments/$FOLDER_NAME
-  rm ${TRAINING_LOG_NAME}.test ${TRAINING_LOG_NAME}.train
-fi
+cleanup "Training finished"
+
